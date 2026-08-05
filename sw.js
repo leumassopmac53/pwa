58e239203fbab3d815a039650439d1d783b4f252
leumassopmac53 <leumassopmac53@gmail.com>
2026-08-05 19:35:50 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE="pwa-v1";
+
+const arquivos=[
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install",e=>{
+
+    e.waitUntil(
+
+        caches.open(CACHE)
+        .then(cache=>cache.addAll(arquivos))
+
+    );
+
+});
+
+self.addEventListener("fetch",e=>{
+
+    e.respondWith(
+
+        caches.match(e.request)
+        .then(resp=>resp || fetch(e.request))
+
+    );
+
+});
